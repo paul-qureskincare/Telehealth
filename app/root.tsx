3ef34a536ad06bb11f7d0a27a93fc3c370ef8b0b
speedup-guru <paul@speedup.guru>
2026-01-23 14:02:30 +0400

    done: integrate Savvy embeddables script and component | 2026/01/23 14:02:30

--- app/root.tsx
+++ app/root.tsx
@@ -31,8 +31,44 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.initEmbeddables = () => {
+                const engineDomain = new URL(window.location.href).searchParams.get('embeddables_engine_domain') || 'engine.embeddables.com'
+                const urlRoot = engineDomain.startsWith('http') ? engineDomain : 'https://' + engineDomain
+                const script = document.createElement('script')
+                script.src = \`\${urlRoot}/bundle.js\`
+                document.head.appendChild(script)
+                const initializeEmbeddables = function () {
+                  const allUserData = JSON.parse(localStorage.getItem('SavvyFormUserData') || '{}')
+                  const embeddablesToLoad = [...document.querySelectorAll('savvy, embeddable')].map((el) => {
+                    const attrs = Object.fromEntries([...el.attributes].map((a) => [a.name, a.value]))
+                    const flowId = attrs.id
+                    if (flowId && allUserData[flowId]) {
+                      attrs.userData = Object.fromEntries(Object.entries(allUserData[flowId]||{}).filter(([sk])=>sk==='current_page_id'||sk.startsWith('split_')))
+                    }
+                    return attrs
+                  })
+                  const originUrl = window.location.href
+                  const url = \`\${urlRoot}/init?load=\` + encodeURIComponent(JSON.stringify({ embeddablesToLoad, originUrl }))
+                  fetch(url)
+                    .then((res) => res.json())
+                    .then((response) => eval('(' + response.init_js + ')(response.embeddables_data)'))
+                }
+                if (document.readyState === 'loading') {
+                  document.addEventListener('DOMContentLoaded', initializeEmbeddables)
+                } else {
+                  initializeEmbeddables()
+                }
+              }
+              window.initEmbeddables()
+            `,
+          }}
+        />
       </head>
       <body>
+        <savvy id="flow_2571d52dhga9i00bfhde72a48gj"></savvy>
         {children}
         <ScrollRestoration />
         <Scripts />
