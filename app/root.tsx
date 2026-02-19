54d026aa3b3ddc1cda03049c4e70aac2bba17f03
speedup-guru <paul@speedup.guru>
2026-02-19 19:13:32 +0400

    done: add Google Tag Manager script (GTM-5Z7G3WZC) before closing head tag | 2026/02/19 00:00:00

--- app/root.tsx
+++ app/root.tsx
@@ -144,6 +144,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
             `,
           }}
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5Z7G3WZC');`,
+          }}
+        />
       </head>
       <body>
         {children}
